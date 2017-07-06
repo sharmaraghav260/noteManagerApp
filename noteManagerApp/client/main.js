863585652f9de9ef2f230174797f979bdbb6e2ae
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-07-05 19:20:00 -0600

    Initial setup complete

---
+++ noteManagerApp/client/main.js
@@ -0,0 +1,18 @@
+import {
+  Template
+} from 'meteor/templating';
+
+import './main.html';
+
+Template.body.helpers({
+  notes: [{
+      text: 'My Note 1'
+    },
+    {
+      text: 'My Note 2'
+    },
+    {
+      text: 'My Note 3'
+    },
+  ]
+});
